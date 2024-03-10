758ebccd158fc088d8fadab32c0a73b44461660d
Sagar Medtiya <sagarmedtiya31@gmail.com>
2024-03-10 15:27:42 +0530

    add mongodb connection and few routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
 dotenv.config();
-const port = process.env.PORT || 5000;
+import { notFound , errorHandler} from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+const port = process.env.PORT || 6000;
+
 
 
 const app = express();
-app.use('/api/users', userRoutes)
+
 app.get('/',(req,res)=>{
     res.send('Server is ready')
 })
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, ()=>console.log(`Server started on port ${port}`));
 
 /**
