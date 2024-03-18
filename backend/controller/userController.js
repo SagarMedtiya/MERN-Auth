b9f55b86c4fc852c3d8d53009c9fea6fb85d2953
Sagar Medtiya <sagarmedtiya31@gmail.com>
2024-03-18 19:56:55 +0530

    add authUser route function to validate the email and password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,10 +1,25 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js';
 // @desc   Auth user/set token
 //route    POST /api/users/auth
 //@access  Public
 const authUser = asyncHandler(async (req, res) =>{
-    res.status(200).json({ message: 'Auth User' })
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if(user && (await user.matchPassword(password))){
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password
+        });
+    }else{
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc   Register a new user 
@@ -27,6 +42,7 @@ const registerUser = asyncHandler(async (req, res) =>{
     });
 
     if(user){
+        generateToken(res, user._id)
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -36,7 +52,6 @@ const registerUser = asyncHandler(async (req, res) =>{
     }else{
         res.status(400);
         throw new Error('Invalid user data');
-        
     }
 });
 
