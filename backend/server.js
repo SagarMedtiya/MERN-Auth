f2bd2e54bff0df50709101bb22083c35985fd0fc
Sagar Medtiya <sagarmedtiya31@gmail.com>
2024-03-19 17:40:55 +0530

    add cookie parser function

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import { notFound , errorHandler} from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
@@ -16,6 +17,7 @@ app.get('/',(req,res)=>{
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
